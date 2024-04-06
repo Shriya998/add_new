9c2c0cc51c59c878446e0fc3c5d29538fd733651
shriyah <shriya.hangloo@tekmindz.com>
2024-04-07 00:47:26 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Git Commit Project</h1>
+      <h4>This is the project to commit the code on git hub</h4>
+
+    </div>
+  );
+}
+
+export default App;
